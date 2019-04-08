5c7e8972e4f62209c8b35136d64297dd08ec4f1d
Bryan Yaggi <bryan.yaggi@gmail.com>
2019-04-07 22:35:44 -0700

    Nearly completed Problem 3. Two implementations of 3g are still over time limit.

--- foundations/submission.py
+++ foundations/submission.py
@@ -12,7 +12,8 @@ def findAlphabeticallyLastWord(text):
     You might find max() and list comprehensions handy here.
     """
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    words = text.split()
+    return max(words)
     # END_YOUR_CODE
 
 ############################################################
@@ -24,7 +25,7 @@ def euclideanDistance(loc1, loc2):
     are pairs of numbers (e.g., (3, 5)).
     """
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    return math.sqrt((loc2[0] - loc1[0])**2 + (loc2[1] - loc1[1])**2)
     # END_YOUR_CODE
 
 ############################################################
@@ -50,7 +51,39 @@ def mutateSentences(sentence):
                 (reordered versions of this list are allowed)
     """
     # BEGIN_YOUR_CODE (our solution is 20 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    originalSentence = sentence.split()
+    # build dictionary storing a word's predecessors and followers
+    words = {}
+    for index in range(len(originalSentence)):
+        if not(originalSentence[index] in words): # add word if not already in dictionary
+            words[originalSentence[index]] = (set(), set())
+        if index != 0: # append preceding word to word's preceding list
+            words[originalSentence[index]][0].add(originalSentence[index-1])
+        if index != len(originalSentence) - 1: # append following word word's following list
+            words[originalSentence[index]][1].add(originalSentence[index+1])
+
+    phrases = collections.deque()
+    for word in words:
+        phrases.append([word])
+
+    if len(phrases) == 0:
+        return []
+
+    phrase = phrases.popleft()
+    while len(phrase) < len(originalSentence):
+        lastWord = phrase[-1]
+        for followingWord in words[lastWord][1]:
+            newPhrase = list(phrase)
+            newPhrase.append(followingWord)
+            phrases.append(newPhrase)
+        phrase = phrases.popleft()
+
+    output = [' '.join(phrase)]
+    while len(phrases) > 0:
+        phrase = phrases.popleft()
+        output.append(' '.join(phrase))
+
+    return output
     # END_YOUR_CODE
 
 ############################################################
@@ -64,7 +97,10 @@ def sparseVectorDotProduct(v1, v2):
     This function will be useful later for linear classifiers.
     """
     # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    dotProduct = 0
+    for key in v1:
+        dotProduct += v1[key] * v2[key]
+    return dotProduct
     # END_YOUR_CODE
 
 ############################################################
@@ -76,7 +112,9 @@ def incrementSparseVector(v1, scale, v2):
     This function will be useful later for linear classifiers.
     """
     # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    allKeys = list(set(v1.keys()) | set(v2.keys()))
+    for key in allKeys:
+        v1[key] += scale * v2[key]
     # END_YOUR_CODE
 
 ############################################################
@@ -89,12 +127,59 @@ def findSingletonWords(text):
     You might find it useful to use collections.defaultdict(int).
     """
     # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    words = text.split()
+    counter = collections.Counter(words)
+    singletons = set()
+    for key in counter:
+        if counter[key] == 1:
+            singletons.add(key)
+    return singletons
     # END_YOUR_CODE
 
 ############################################################
 # Problem 3g
+def computeLongestPalindromeLength_v1(text):
+    """
+    A palindrome is a string that is equal to its reverse (e.g., 'ana').
+    Compute the length of the longest palindrome that can be obtained by deleting
+    letters from |text|.
+    For example: the longest palindrome in 'animal' is 'ama'.
+    Your algorithm should run in O(len(text)^2) time.
+    You should first define a recurrence before you start coding.
+    """
+    # BEGIN_YOUR_CODE (our solution is 19 lines of code, but don't worry if you deviate from this)
+    if len(text) == 0:
+        return 0
+    if len(text) == 1:
+        return 1
+    if text == text[::-1]:
+        return len(text)
 
+    # find duplicate characters
+    duplicateCharacters = []
+    counter = collections.Counter(text)
+    for character in counter:
+        if counter[character] >= 2:
+            duplicateCharacters.append(character)
+
+    if len(duplicateCharacters) == 0:
+        return 1
+
+    # create remnant strings by removing duplicates at end
+    remnants = []
+    for character in duplicateCharacters:
+        remnants.append(text[text.find(character)+1:text.rfind(character)])
+
+    maxLength = 0
+    for remnant in remnants:
+        length = computeLongestPalindromeLength(remnant) + 2
+        if length > maxLength:
+            maxLength = length
+
+    return maxLength
+    # END_YOUR_CODE
+############################################################
+# Problem 3g
 def computeLongestPalindromeLength(text):
     """
     A palindrome is a string that is equal to its reverse (e.g., 'ana').
@@ -105,5 +190,25 @@ def computeLongestPalindromeLength(text):
     You should first define a recurrence before you start coding.
     """
     # BEGIN_YOUR_CODE (our solution is 19 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    cache = {}
+    
+    def removeChar(txt, index):
+        return txt[:index] + txt[index+1:]
+
+    def computeLengthRecursively(txt, firstIndex, lastIndex):
+        if txt in cache:
+            return cache[txt]
+        elif txt == txt[::-1]:
+            result = len(txt)
+        elif txt[firstIndex] == txt[lastIndex]:
+            result = computeLengthRecursively(txt, firstIndex+1, lastIndex-1)
+        else:
+            len1 = computeLengthRecursively(removeChar(txt, firstIndex), firstIndex, lastIndex-1)
+            len2 = computeLengthRecursively(removeChar(txt, lastIndex), firstIndex, lastIndex-1)
+            result =  max(len1,len2)
+        
+        cache[txt] = result
+        return result
+
+    return computeLengthRecursively(text, 0, len(text)-1)
     # END_YOUR_CODE
---
+++ foundations/submissionTest.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python2
+
+import collections
+
+import submission
+
+def test3a():
+    print(submission.findAlphabeticallyLastWord('ball fall tall yall'))
+
+def test3b():
+    print(submission.euclideanDistance((1,5), (4,1)))
+
+def test3c():
+    print(submission.mutateSentences('the cat and the mouse'))
+    print(submission.mutateSentences('a a a a a'))
+
+def test3d():
+    print(submission.sparseVectorDotProduct(collections.defaultdict(float, {'a':5}), collections.defaultdict(float, {'b':2, 'a':3})))
+
+def test3e():
+    v1 = collections.defaultdict(float, {'a':5})
+    v2 = collections.defaultdict(float, {'b':2, 'a':3})
+    print(submission.incrementSparseVector(v1, 2, v2))
+
+def test3g():
+    print(submission.computeLongestPalindromeLength('animal'))
+    print(submission.computeLongestPalindromeLength_v1('animal'))
+
+if __name__ == '__main__':
+    #test3a()
+    #test3b()
+    #test3d()
+    #test3e()
+    #test3c()
+    test3g()
